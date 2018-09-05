d7ac2f1a2bcfec5dbc242caec8902fbdcfc5ea15
shirley <domo@shirleys-MacBook-Air.local>
2018-09-05 07:29:26 -0400

    Cleanup

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -11,21 +11,6 @@ const routes = require('../routes.js');
 
 app.use('/retire', routes);
 
-passport.use(new LocalStrategy(function(username, password, done) {
-  knex.select('username').from('users')
-  .where({username : username})
-  .asCallback(function(err, user) {
-    if (err) {console.log(err)}
-    if (!user) {
-      return done(null, false, { message: 'No such user.'})
-    }
-    if (user.password !== password) {
-      return done(null, false, { message: 'Incorrect password.'})
-    }
-    return done(null, user)
-  })
-}))
-
 var port = process.env.PORT || 3000;
 
 app.listen(3000, () => console.log(`Example app listening on port ${port}!`));
