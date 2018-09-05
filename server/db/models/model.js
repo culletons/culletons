c4eab8ed16159ea88b67f6588848ea1e51cc4687
Kenni Silverio <ksilverio31@gmail.com>
2018-09-05 17:43:43 -0400

    adding models and controllers for getUser and createUser

--- server/db/models/model.js
+++ server/db/models/model.js
@@ -1,1 +1,49 @@
+const db = require('../connection')
 
+
+var User = db.Model.extend({
+    tablename: 'users',
+    hasTimeStamps: true,
+    // verifyPassword: function(password) {
+    //     return this.get('password') === password;
+    // },
+    // byEmail: function(email) {
+    //     return this.forge().query({where:{ email: email }}).fetch();
+    // }
+
+})
+
+var Users = db.Collection.extend({  
+    model: User
+});
+
+var getUserFromDB = (username) => {
+    db.knex('users').where({username: username}).fetch()
+    .then(user => {
+        return user;
+    })
+    .catch(err => {
+        console.log("this error occurred in getUserFromDB ", err);
+    })
+}
+
+var createUserInDB = (username, fullName, password, email) => {
+    if(!getUserFromDB(username)){
+        db.knex('users').insert({username: username}, {fullName: fullName}, {password: password}, {email: email})
+        .then(newUser => {
+            console.log(newUser, " was created in the database model.")
+        })
+        .catch(err => {
+            console.log("this error occurred in createUserInDB ", err);
+        })
+    }
+}
+
+var updateUserInDB = (update) => {
+}
+
+var userLoginDB = (req, res) => {
+
+}
+
+module.exports = {User, Users, getUserFromDB, createUserInDB, updateUserInDB, userLoginDB}
