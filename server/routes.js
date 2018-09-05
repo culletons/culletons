1f8fc22bea74bf8ae3f482a0739e8e4f82a4ae95
shirley <domo@shirleys-MacBook-Air.local>
2018-09-05 15:37:28 -0400

    Routing/sessions initialized

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const usersController = require('./controllers/controller.js');
+const passport = require ('passport')
 
 router.get('/users', usersController.getUser);
 router.post('/users', usersController.createUser);
 router.put('/users', usersController.updateUser);
 
-router.post('/login', usersController.userLogin);
+router.post('/login', passport.authenticate('local'), usersController.userLogin);
 
 module.exports = router;
