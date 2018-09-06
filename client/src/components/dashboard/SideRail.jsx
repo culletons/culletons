426e97507c5f3d8568a94db8569a7b5c99849a16
shirley <domo@shirleys-MacBook-Air.local>
2018-09-06 11:01:38 -0400

    Added HighCharts and stylized landing page

--- client/src/components/dashboard/SideRail.jsx
+++ client/src/components/dashboard/SideRail.jsx
@@ -16,13 +16,22 @@ class Dashboard extends React.Component {
 
   render() {
     return (
+
       <div className="card side-rail">
         <div className="card-body">
           <h3 className="card-title">Your Profile:</h3>
           <br/>
+
+
+<ul className="list-group">
+  <li className="list-group-item">Basic Info</li>
+  <li className="list-group-item">Monthly Budget</li>
+  <li className="list-group-item">..</li>
+  <li className="list-group-item">..</li>
+</ul>
           
         </div>
-      </div>
+     </div>
     );
   }
 
