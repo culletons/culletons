3e194f1fa08ba95a32e2c6df8f972fab08ce39b3
Trent Going <trent.going@gmail.com>
2018-09-06 13:10:29 -0400

    plaid UI and initial server integration

--- client/src/components/dashboard/SideRail.jsx
+++ client/src/components/dashboard/SideRail.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import axios from 'axios';
 import BasicInfo from './BasicInfo.jsx';
 
 class Dashboard extends React.Component {
@@ -8,10 +8,38 @@ class Dashboard extends React.Component {
     this.state = {
       user: null
     }
+
+    this.launchPlaidLink = this.launchPlaidLink.bind(this);
   }
 
   componentDidMount() {
-    // GET USER INFO NOW
+    // Plaid app needs to initialize it's "Link" feature through which accounts can be added. https://plaid.com/docs/#integrating-with-link
+    this.handler = Plaid.create({
+      apiVersion: 'v2',
+      clientName: 'Plaid Walkthrough Demo',
+      env: 'sandbox',
+      product: ['transactions'],
+      key: '2dec9b90cfcc7a2d76b295ac1b3504',
+      onSuccess: function(public_token, metadata) {   // Defining what should happen upon success of the Plaid 'Link' feature, at minimum, send public_token to our server.
+        console.log("HEY THIS IS THE RESPONSE FROM PLAID: " + public_token);
+        axios.post('/retire/get_access_token', {
+         
+            public_token: public_token,
+            metadata: metadata
+          
+        })
+        .then(() => {
+          // HERE WE MANIPULATE STATE TO SHOW THAT IT WAS SUCCESSFUL
+        })
+        .catch((err) => {
+          console.log('Plaid didnt work!' + err);
+        })
+      },
+    });
+  }
+
+  launchPlaidLink() {
+    this.handler.open();
   }
 
   render() {
@@ -20,7 +48,7 @@ class Dashboard extends React.Component {
         <div className="card-body">
           <h3 className="card-title">Your Profile:</h3>
           <br/>
-          
+          <button id="link-btn" className="btn btn-success" onClick={this.launchPlaidLink}>Link Account</button>
         </div>
       </div>
     );
--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const usersController = require('./controllers/controller.js');
-const passport = require ('passport')
+const passport = require ('passport');
+const plaidController = require('./controllers/plaid.js');
 
 router.get('/users', usersController.getUser);
 router.post('/users', usersController.createUser);
 router.put('/users', usersController.updateUser);
 
 router.post('/login', passport.authenticate('local'), usersController.userLogin);
+
+router.post('/get_access_token', plaidController.getAccessToken);
 
 module.exports = router;
