0e606837e54c6b1e0c731bb4d8c1762e68b7de4d
Kenni Silverio <ksilverio31@gmail.com>
2018-09-06 14:38:10 -0400

    adding models routes and controllers for plans, items, and extra functionality to getUser to search by Oauth token

--- server/routes.js
+++ server/routes.js
@@ -5,5 +5,12 @@ router.get('/users', usersController.getUser);
 router.post('/users', usersController.createUser);
 router.put('/users', usersController.updateUser);
 
+router.get('/plans', usersController.getPlans)
+router.post('/plans', usersController.createPlan);
+router.put('/plans', usersController.updatePlan);
+
+router.get('/items', usersController.getItems)
+router.post('/items', usersController.createItem);
+router.put('/items', usersController.updateItem); //optional?
 
 module.exports = router;
